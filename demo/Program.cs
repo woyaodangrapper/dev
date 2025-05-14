4269f483af8300e6706837097fb9d8069c0dcd18
BUG killer：） <38523332+woyaodangrapper@users.noreply.github.com>
2025-05-14 14:26:43 +0800

    add using

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicaitonHostedServices();
